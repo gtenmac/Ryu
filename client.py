6f3c353595c4cd0879d1b940410e98c348dd3d49
陳書慕 <d757821137@gmail.com>
2018-05-11 15:22:45 +0800

    更新

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 from scapy.all import send,IP,load_contrib
 
 load_contrib('mpls')
-p = IP(dst='10.0.0.4')/MPLS()
-
-send(p,count=4)
+p = Ether()/IP(dst='10.0.0.4')
+p = MPLS(p)
+print(p.show())
+# send(p,count=4)
