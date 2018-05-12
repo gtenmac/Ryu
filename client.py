8ef5ddbf8e8172c0a456f7c2278b61bcdef18dc3
gtenmac <32523064+gtenmac@users.noreply.github.com>
2018-05-12 17:20:53 +0800

    更新

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from scapy.all import send,IP,load_contrib,Ether
+from scapy.all import send,IP,load_contrib
 
 load_contrib('mpls')
-p = Ether()/IP(dst='10.0.0.4')
+p = Ether(type=34887)/IP(dst='10.0.0.2')/MPLS()
 p = MPLS(p)
-print(p.show())
-# send(p,count=4)
+# print(p.show())
+send(p,count=4)
